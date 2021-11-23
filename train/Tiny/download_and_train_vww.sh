7fb155998f93c5e4f9d2ac85e91e273de1e9e9df
KYC127 <48679612+KYE2138@users.noreply.github.com>
2021-11-23 19:53:00 +0800

    add excute ConvertToC.ipynb

--- train/Tiny/download_and_train_vww.sh
+++ train/Tiny/download_and_train_vww.sh
@@ -7,3 +7,4 @@ tar -xvf vw_coco2014_96.tar.gz
 #python3 parse_coco.py annotations/instances_train2017.json
 python3 train_vww.py
 python3 convert_vww.py trained_models/vww_96.h5
+runipy -o ConvertToC.ipynb
